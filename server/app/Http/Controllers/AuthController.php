8c0554150286e5ed6f99e05e5352438ce95646bd
My name <ekkomain93@gmail.com>
2024-09-16 22:59:37 +0100

    Adding controller && routes for authentication

---
+++ server/app/Http/Controllers/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+
+
+class AuthController extends Controller
+{
+    // Register method
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|unique:users|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'User registered successfully!',
+            'user' => $user,
+        ]);
+    }
+
+    // Login method
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ]);
+        }
+
+        // Return a success message
+        return response()->json([
+            'message' => 'Logged in successfully!',
+            'user' => Auth::user(),
+        ]);
+    }
+
+    // Logout method
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully!',
+        ]);
+    }
+}
